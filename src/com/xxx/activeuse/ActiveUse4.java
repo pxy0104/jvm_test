e6a087ca831648d5fa65137b6245009b26658a17
pxy <benlaixy@163.com>
2023-04-25 16:34:56 +0800

    类的主动使用

--- src/com/xxx/activeuse/ActiveUse4.java
+++ src/com/xxx/activeuse/ActiveUse4.java
@@ -6,26 +6,28 @@ import org.junit.Test;
 /**
  * 2023/4/25
  **/
-interface People{
-    public static final Thread t = new Thread(){
+interface People {
+    public static final Thread t = new Thread() {
         {
             System.out.println("初始化接口People");
         }
     };
+
     public default void speak() {
         System.out.println("说话");
     }
 }
 
-class Son implements People{
+class Son implements People {
     static {
         System.out.println("Son 初始化");
     }
+
     public static int NUM = 1;
 }
 
-class Daughter extends Son{
-    {
+class Daughter extends Son {
+    static {
         System.out.println("Daughter is initialized.");
     }
 
@@ -33,16 +35,19 @@ class Daughter extends Son{
 
 
 }
+
 public class ActiveUse4 {
-    {
+    static {
         System.out.println("ActiveUse4主类初始化");
     }
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(Daughter.A);
     }
+
     @Test
-    public void test(){
+    public void test() {
+        System.out.println(Son.NUM);
         System.out.println(Daughter.A);
     }
 }
